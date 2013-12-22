da49dd83c215e6d1a434e68c0f7824c127a1fb25
faizaanshamsi <faizaanshamsi@gmail.com>
2013-12-22 16:15:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JournalEntry::Application.config.secret_key_base = 'c6afded28489dc9bf0b5f93d54e18cf3ef7393cf9373cd16d1d430b21a8436fabce83736d681bbe0d078ed10359cd6798ac9390fdb153e519fc3aaa0da6abe67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JournalEntry::Application.config.session_store :cookie_store, key: '_journal_entry_session'
